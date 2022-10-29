0c2bd21b708e54ec1413fa883771d35f0df08c83
Eric Arbour <arboureric@gmail.com>
2022-10-29 16:03:14 -0400

    Add auth flow to frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import Head from "next/head";
 import styled from "styled-components";
+import { signIn, signOut } from "next-auth/react";
 
-export default function Home(): JSX.Element {
+import { authOptions } from "./api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next";
+import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
+
+interface HomeProps {
+  session: Session | null;
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async (
+  context
+) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
+
+export default function Home({ session }: HomeProps): JSX.Element {
   return (
     <Container>
       <Head>
@@ -9,31 +35,52 @@ export default function Home(): JSX.Element {
         <meta name="description" content="A simple RSS player for podcasts." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main>
-        <Title>RSS Player</Title>
-        <Description>A simple RSS audio player for podcasts.</Description>
-        <Grid>
-          <Card href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </Card>
-          <Card href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </Card>
-          <Card href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </Card>
-          <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </Card>
-        </Grid>
-      </Main>
+      <Content session={session} />
     </Container>
+  );
+}
+
+function Content({ session }: HomeProps): JSX.Element {
+  if (!session) {
+    return (
+      <Main centerVertically>
+        <Title big>RSS Player</Title>
+        <Description>A simple RSS audio player for podcasts.</Description>
+        <Button onClick={(): Promise<undefined> => signIn()}>Sign in</Button>
+      </Main>
+    );
+  }
+
+  return (
+    <Main>
+      <Header>
+        <Title>RSS Player</Title>
+        <SignedInUser>
+          <p>Hello {session.user?.email}!</p>
+          <Button onClick={(): Promise<undefined> => signOut()}>
+            Sign out
+          </Button>
+        </SignedInUser>
+      </Header>
+      <Grid>
+        <Card href="https://nextjs.org/docs">
+          <h2>Documentation &rarr;</h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </Card>
+        <Card href="https://nextjs.org/learn">
+          <h2>Learn &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </Card>
+        <Card href="https://github.com/vercel/next.js/tree/canary/examples">
+          <h2>Examples &rarr;</h2>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </Card>
+        <Card href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+          <h2>Deploy &rarr;</h2>
+          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+        </Card>
+      </Grid>
+    </Main>
   );
 }
 
@@ -41,20 +88,42 @@ const Container = styled.div`
   padding: 0 2rem;
 `;
 
-const Main = styled.main`
+interface MainProps {
+  centerVertically?: true;
+}
+
+const Main = styled.main<MainProps>`
   min-height: 100vh;
-  padding: 4rem 0;
-  flex: 1;
+  padding: 1rem 0;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: ${({ centerVertically }): string =>
+    centerVertically ? "center" : "flex-start"};
   align-items: center;
 `;
 
-const Title = styled.h1`
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid #eaeaea;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SignedInUser = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+interface TitleProps {
+  big?: true;
+}
+
+const Title = styled.h1<TitleProps>`
   margin: 0;
   line-height: 1.15;
-  font-size: 4rem;
+  font-size: ${({ big }): string => (big ? "4rem" : "2rem")};
   text-align: center;
 `;
 
@@ -71,6 +140,7 @@ const Grid = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   max-width: 800px;
+  margin-top: 1.5rem;
 `;
 
 const Card = styled.a`
@@ -94,6 +164,25 @@ const Card = styled.a`
     font-size: 1.25rem;
     line-height: 1.5;
   }
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    border-color: #222;
+  }
+`;
+
+const Button = styled.button`
+  background-color: black;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border: 1px solid #eaeaea;
+  border-radius: 3px;
 
   &:hover,
   &:focus,
