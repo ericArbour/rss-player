2f44d95abfb435ca1eda81605e9851f1fdf22979
Eric Arbour <arboureric@gmail.com>
2022-10-29 14:19:00 -0400

    Add NextAuth email provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,21 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import EmailProvider from "next-auth/providers/email";
 import { sequelize } from "../../../db/models";
 import SequelizeAdapter from "../../../db/models/next-auth-adapter";
 
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
+    EmailProvider({
+      server: {
+        host: process.env["EMAIL_SERVER_HOST"],
+        port: process.env["EMAIL_SERVER_PORT"],
+        auth: {
+          user: process.env["EMAIL_SERVER_USER"],
+          pass: process.env["EMAIL_SERVER_PASSWORD"],
+        },
+      },
+      from: process.env["EMAIL_FROM"],
+    }),
   ],
   adapter: SequelizeAdapter(sequelize),
 };
